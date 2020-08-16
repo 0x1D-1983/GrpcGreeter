c63e04b8e6e91794fd1c397280b687100ace9187
Janos <kelemen.janos@gmail.com>
2020-08-16 11:58:06 +0100

    Grpc Server, Client, API initial working solution

---
+++ GrpcGreeterApi/Controllers/GreeterController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using GrpcGreeter;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcGreeterApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GreeterController : ControllerBase
+    {
+        private readonly ILogger<GreeterController> _logger;
+        private readonly Greeter.GreeterClient _client;
+
+        public GreeterController(ILogger<GreeterController> logger, Greeter.GreeterClient client)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            
+
+            var response = await _client.SayHelloAsync(new HelloRequest
+            {
+                Name = "Janos"
+            });
+
+            return response.Message;
+        }
+    }
+}
---
+++ GrpcGreeterClient/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using GrpcGreeter;
+
+namespace GrpcGreeterClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Call insecure gRPC services with .NET Core client
+            AppContext.SetSwitch("System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport", true);
+
+            using var channel = GrpcChannel.ForAddress("http://localhost:5000");
+
+            var client = new Greeter.GreeterClient(channel);
+
+            var reply = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = "Janos"
+            });
+
+            Console.WriteLine($"Greeting: {reply.Message}");
+
+            Console.ReadKey();
+        }
+    }
+}
